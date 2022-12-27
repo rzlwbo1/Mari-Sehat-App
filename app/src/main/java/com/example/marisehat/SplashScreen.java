8fb00c28898ecd1f146fe1461ea430c39bfec499
rzlwbo1 <rizalwb2@gmail.com>
2022-12-27 08:51:37 +0700

    splash screen

---
+++ app/src/main/java/com/example/marisehat/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.marisehat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toHome = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(toHome);
+                finish();
+            }
+        }, 3000);
+    }
+}
